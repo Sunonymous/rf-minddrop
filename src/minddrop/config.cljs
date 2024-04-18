16b2b612d5b3a3c3e43d4dab52c20ae80c52e589
Sunny <embodiedsunshine@gmail.com>
2024-04-18 10:44:18 -0600

    created some base settings

--- src/minddrop/config.cljs
+++ src/minddrop/config.cljs
@@ -2,3 +2,16 @@
 
 (def debug?
   ^boolean goog.DEBUG)
+
+(def config-local-storage-key "minddrop_config")
+
+(def settings-description
+  {:user                        "Currently unused. String name of user."
+   :prefill-relabel             "Prefills the relabel dialog with the current label when true."
+   :navigate-from-focused-drops "Allows entering and exiting drops from focused mode."
+   :confirm-before-action       "Ensures the user wishes to perform an action before proceeding."})
+
+(def default-config {:user                        "me"
+                     :prefill-relabel             true
+                     :navigate-from-focused-drops false
+                     :confirm-before-action       false})
